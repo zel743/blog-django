74c1b6b79cbfa435640ca120b05848f1aac0bd87
zel743 <frv903@outlook.com>
2025-03-14 17:22:27 -0600

    changes

--- myblog/urls.py
+++ myblog/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
-    path("index/", include("blog.urls")),
-    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
     ]
