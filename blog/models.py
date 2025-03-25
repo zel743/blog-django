0f1c52c5523b4b29be1ebd0e6b43b1b56cd9493c
zel743 <frv903@outlook.com>
2025-03-25 00:17:32 -0600

    post page

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    SECTION_CHOICES = [
+        ('linux', 'Linux'),
+        ('windows', 'Windows'),
+        ('macos', 'macOS'),
+    ]
+    
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    image = models.ImageField(upload_to='posts/', blank=True, null=True)  # Requiere Pillow
+    section = models.CharField(max_length=20, choices=SECTION_CHOICES)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
